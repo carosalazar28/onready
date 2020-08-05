4c938aa56b4ded8e4e4bbcaf89a42ecb90176732
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-08-04 19:06:25 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const autos = [
+    {marca:'Peugeot', modelo: 206, puertas: 4, cilindrada: '', precio: 200.000}, 
+    {marca:'Honda', modelo: 'Titan', puertas: '', cilindrada: '125c', precio: 60.000}, 
+    {marca: 'Peugeot', modelo: 208, puertas: 5, cilindrada: '', precio: 250.000}, 
+    {marca: 'Yamaha', modelo: 'YBR', puertas: '', cilindrada: '160c', precio: 80.500}
+]
+
+const autosMasCaros = arrayAutos.forEach(function(auto) {
+    return arrayAutos.precio > 200.000
+})
+
+console.log(autosMasCaros)
